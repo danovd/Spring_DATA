c7920c03998107f1c1949e4a3f66b21051c41fb0
danovd <danovski_dimitar@abv.bg>
2023-07-22 09:53:41 +0300

    apartments XML processing functionality created

--- EXAMS/DB_EXAM_02Apr2022_RealEstate/real_estate/src/main/java/softuni/exam/service/impl/ApartmentServiceImpl.java
+++ EXAMS/DB_EXAM_02Apr2022_RealEstate/real_estate/src/main/java/softuni/exam/service/impl/ApartmentServiceImpl.java
@@ -1,20 +1,56 @@
 package softuni.exam.service.impl;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
+import softuni.exam.models.dto.ImportApartmentRootDTO;
+import softuni.exam.repository.ApartmentRepository;
 import softuni.exam.service.ApartmentService;
 
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 @Service
 public class ApartmentServiceImpl implements ApartmentService {
+
+
+    private static final String APARTMENTS_FILE_PATH = "src/main/resources/files/xml/apartments.xml";
+
+    private final ApartmentRepository apartmentRepository;
+
+    private final ModelMapper modelMapper;
+    private final Validator validator;
+
+    private final Unmarshaller unmarshaller;
+
+    public ApartmentServiceImpl(ApartmentRepository apartmentRepository) throws JAXBException {
+        this.apartmentRepository = apartmentRepository;
+
+        JAXBContext context = JAXBContext.newInstance(ImportApartmentRootDTO.class);
+        this.unmarshaller = context.createUnmarshaller();
+
+        this.validator = Validation
+                .buildDefaultValidatorFactory()
+                .getValidator();
+
+        this.modelMapper = new ModelMapper();
+
+    }
+
+
     @Override
     public boolean areImported() {
-        return false;
+        return apartmentRepository.count() > 0;
     }
 
     @Override
     public String readApartmentsFromFile() throws IOException {
-        return null;
+        return Files.readString(Path.of(APARTMENTS_FILE_PATH));
     }
 
     @Override
