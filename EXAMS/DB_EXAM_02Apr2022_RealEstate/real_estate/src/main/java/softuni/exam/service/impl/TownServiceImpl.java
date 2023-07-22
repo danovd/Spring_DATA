fc07b2ef1fa643c74e45c444206d4d6256e569cc
danovd <danovski_dimitar@abv.bg>
2023-07-22 09:48:08 +0300

    towns JSON processing functionality established

--- EXAMS/DB_EXAM_02Apr2022_RealEstate/real_estate/src/main/java/softuni/exam/service/impl/TownServiceImpl.java
+++ EXAMS/DB_EXAM_02Apr2022_RealEstate/real_estate/src/main/java/softuni/exam/service/impl/TownServiceImpl.java
@@ -1,19 +1,52 @@
 package softuni.exam.service.impl;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
+import softuni.exam.repository.TownRepository;
 import softuni.exam.service.TownService;
 
+import javax.validation.Validation;
+import javax.validation.Validator;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 @Service
 public class TownServiceImpl implements TownService {
+
+
+    private static final String TOWNS_FILE_PATH = "src/main/resources/files/json/towns.json";
+
+    private final TownRepository townRepository;
+
+    private final ModelMapper modelMapper;
+    private final Gson gson;
+    private final Validator validator;
+
+    public TownServiceImpl(TownRepository townRepository) {
+        this.townRepository = townRepository;
+
+        this.gson = new GsonBuilder().setPrettyPrinting().create();
+
+        this.validator = Validation
+                .buildDefaultValidatorFactory()
+                .getValidator();
+
+        this.modelMapper = new ModelMapper();
+
+    }
+
+
     @Override
     public boolean areImported() {
-        return false;
+        return townRepository.count() > 0;
     }
 
     @Override
     public String readTownsFileContent() throws IOException {
-        return null;
+        return Files.readString(Path.of(TOWNS_FILE_PATH));
     }
 
     @Override
