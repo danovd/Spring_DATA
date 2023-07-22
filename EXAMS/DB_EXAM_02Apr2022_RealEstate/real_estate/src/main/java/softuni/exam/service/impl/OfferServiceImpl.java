0cda9d772793958f49cf22a9972c1876675646f4
danovd <danovski_dimitar@abv.bg>
2023-07-22 10:02:39 +0300

    offers XML processing functionality created

--- EXAMS/DB_EXAM_02Apr2022_RealEstate/real_estate/src/main/java/softuni/exam/service/impl/OfferServiceImpl.java
+++ EXAMS/DB_EXAM_02Apr2022_RealEstate/real_estate/src/main/java/softuni/exam/service/impl/OfferServiceImpl.java
@@ -1,20 +1,70 @@
 package softuni.exam.service.impl;
 
+import org.modelmapper.Converter;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.spi.MappingContext;
 import org.springframework.stereotype.Service;
+import softuni.exam.models.dto.ImportOfferRootDTO;
+import softuni.exam.repository.OfferRepository;
 import softuni.exam.service.OfferService;
 
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 @Service
 public class OfferServiceImpl implements OfferService {
+
+    private static final String OFFERS_FILE_PATH = "src/main/resources/files/xml/offers.xml";
+
+    private final OfferRepository offerRepository;
+
+    private final ModelMapper modelMapper;
+    private final Validator validator;
+
+    private final Unmarshaller unmarshaller;
+
+    public OfferServiceImpl(OfferRepository offerRepository) throws JAXBException {
+        this.offerRepository = offerRepository;
+
+        JAXBContext context = JAXBContext.newInstance(ImportOfferRootDTO.class);
+        this.unmarshaller = context.createUnmarshaller();
+
+        this.validator = Validation
+                .buildDefaultValidatorFactory()
+                .getValidator();
+
+        this.modelMapper = new ModelMapper();
+
+/*
+        modelMapper.addConverter(new Converter<String, LocalDate>() {
+            @Override
+            public LocalDate convert(MappingContext<String, LocalDate> context) {
+                String dateString = context.getSource();
+
+                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+                return LocalDate.parse(dateString, formatter);
+            }
+        });
+    */
+    }
+
+
     @Override
     public boolean areImported() {
-        return false;
+        return offerRepository.count() > 0;
     }
 
     @Override
     public String readOffersFileContent() throws IOException {
-        return null;
+        return Files.readString(Path.of(OFFERS_FILE_PATH));
     }
 
     @Override
