62038f7bd6a9045f6aeb7fb30b8f65429594bb7a
danovd <danovski_dimitar@abv.bg>
2023-07-22 09:44:06 +0300

    agents json processing function established

--- EXAMS/DB_EXAM_02Apr2022_RealEstate/real_estate/src/main/java/softuni/exam/service/impl/AgentServiceImpl.java
+++ EXAMS/DB_EXAM_02Apr2022_RealEstate/real_estate/src/main/java/softuni/exam/service/impl/AgentServiceImpl.java
@@ -1,19 +1,53 @@
 package softuni.exam.service.impl;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
+import softuni.exam.repository.AgentRepository;
 import softuni.exam.service.AgentService;
 
+import javax.validation.Validation;
+import javax.validation.Validator;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 @Service
 public class AgentServiceImpl implements AgentService {
+
+
+    private static final String AGENTS_FILE_PATH = "src/main/resources/files/json/agents.json";
+
+    private final AgentRepository agentRepository;
+
+    private final ModelMapper modelMapper;
+    private final Gson gson;
+    private final Validator validator;
+
+    public AgentServiceImpl(AgentRepository agentRepository) {
+
+        this.agentRepository = agentRepository;
+
+        this.gson = new GsonBuilder().setPrettyPrinting().create();
+
+        this.validator = Validation
+                .buildDefaultValidatorFactory()
+                .getValidator();
+
+        this.modelMapper = new ModelMapper();
+
+    }
+
+
     @Override
     public boolean areImported() {
-        return false;
+        return agentRepository.count() > 0;
     }
 
     @Override
     public String readAgentsFromFile() throws IOException {
-        return null;
+        return Files.readString(Path.of(AGENTS_FILE_PATH));
     }
 
     @Override
