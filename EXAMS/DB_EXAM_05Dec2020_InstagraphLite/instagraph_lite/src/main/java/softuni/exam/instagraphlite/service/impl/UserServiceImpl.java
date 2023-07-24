1c8b5d8d9927be2dc5588d3e2b9174562daba218
danovd <danovski_dimitar@abv.bg>
2023-07-24 08:12:07 +0300

    read JSON users

--- EXAMS/DB_EXAM_05Dec2020_InstagraphLite/instagraph_lite/src/main/java/softuni/exam/instagraphlite/service/impl/UserServiceImpl.java
+++ EXAMS/DB_EXAM_05Dec2020_InstagraphLite/instagraph_lite/src/main/java/softuni/exam/instagraphlite/service/impl/UserServiceImpl.java
@@ -1,21 +1,43 @@
 package softuni.exam.instagraphlite.service.impl;
 
+import com.google.gson.Gson;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
+import softuni.exam.instagraphlite.repository.UserRepository;
 import softuni.exam.instagraphlite.service.UserService;
+import softuni.exam.instagraphlite.util.ValidationUtil;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 
 @Service
 public class UserServiceImpl implements UserService {
+
+    private static final String USERS_FILE_PATH = "src/main/resources/files/users.json";
+
+    private final ModelMapper modelMapper;
+    private final Gson gson;
+    private final ValidationUtil validator;
+    private final UserRepository userRepository;
+
+    public UserServiceImpl(ModelMapper modelMapper, Gson gson, ValidationUtil validator, UserRepository userRepository) {
+        this.modelMapper = modelMapper;
+        this.gson = gson;
+        this.validator = validator;
+        this.userRepository = userRepository;
+    }
+
+
     @Override
     public boolean areImported() {
-        return false;
+        return this.userRepository.count() > 0;
     }
 
     @Override
     public String readFromFileContent() throws IOException {
-        return null;
+        return Files.readString(Path.of(USERS_FILE_PATH));
     }
 
     @Override
