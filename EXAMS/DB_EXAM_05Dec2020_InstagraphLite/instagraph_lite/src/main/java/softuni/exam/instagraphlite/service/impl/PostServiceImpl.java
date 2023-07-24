010cc5213e9a485e274dec4842f2a3cc3b7a9f18
danovd <danovski_dimitar@abv.bg>
2023-07-24 08:23:01 +0300

    read XML posts

--- EXAMS/DB_EXAM_05Dec2020_InstagraphLite/instagraph_lite/src/main/java/softuni/exam/instagraphlite/service/impl/PostServiceImpl.java
+++ EXAMS/DB_EXAM_05Dec2020_InstagraphLite/instagraph_lite/src/main/java/softuni/exam/instagraphlite/service/impl/PostServiceImpl.java
@@ -1,21 +1,40 @@
 package softuni.exam.instagraphlite.service.impl;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
+import softuni.exam.instagraphlite.repository.PostRepository;
 import softuni.exam.instagraphlite.service.PostService;
+import softuni.exam.instagraphlite.util.ValidationUtil;
+import softuni.exam.instagraphlite.util.XmlParser;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 
 @Service
 public class PostServiceImpl implements PostService {
+    private static final String POSTS_FILE_PATH = "src/main/resources/files/posts.xml";
+    private final PostRepository postRepository;
+    private final ModelMapper modelMapper;
+    private final XmlParser xmlParser;
+    private final ValidationUtil validator;
+
+    public PostServiceImpl(PostRepository postRepository, ModelMapper modelMapper, XmlParser xmlParser, ValidationUtil validator) {
+        this.postRepository = postRepository;
+        this.modelMapper = modelMapper;
+        this.xmlParser = xmlParser;
+        this.validator = validator;
+    }
+
     @Override
     public boolean areImported() {
-        return false;
+        return this.postRepository.count() > 0;
     }
 
     @Override
     public String readFromFileContent() throws IOException {
-        return null;
+        return Files.readString(Path.of(POSTS_FILE_PATH));
     }
 
     @Override
