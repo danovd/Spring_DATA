07153399e023b67ee0986dd125b88e34ab60c03c
danovd <danovski_dimitar@abv.bg>
2023-07-25 09:46:47 +0300

    passengers JSON file read

--- EXAMS/DB_EXAM_03Apr2020_Airline/airline/src/main/java/softuni/exam/service/impl/PassengerServiceImpl.java
+++ EXAMS/DB_EXAM_03Apr2020_Airline/airline/src/main/java/softuni/exam/service/impl/PassengerServiceImpl.java
@@ -1,19 +1,39 @@
 package softuni.exam.service.impl;
 
+import com.google.gson.Gson;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
+import softuni.exam.repository.PassengerRepository;
 import softuni.exam.service.PassengerService;
+import softuni.exam.util.ValidationUtil;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 @Service
 public class PassengerServiceImpl implements PassengerService {
+    private static final String PASSENGERS_FILE_PATH = "src/main/resources/files/json/passengers.json";
+    private final PassengerRepository passengerRepository;
+    private final ModelMapper modelMapper;
+    private final Gson gson;
+    private final ValidationUtil validator;
+
+    public PassengerServiceImpl(PassengerRepository passengerRepository, ModelMapper modelMapper, Gson gson, ValidationUtil validator) {
+        this.passengerRepository = passengerRepository;
+        this.modelMapper = modelMapper;
+        this.gson = gson;
+        this.validator = validator;
+    }
+
     @Override
     public boolean areImported() {
-        return false;
+        return this.passengerRepository.count() > 0;
     }
 
     @Override
     public String readPassengersFileContent() throws IOException {
-        return null;
+        return Files.readString(Path.of(PASSENGERS_FILE_PATH));
     }
 
     @Override
