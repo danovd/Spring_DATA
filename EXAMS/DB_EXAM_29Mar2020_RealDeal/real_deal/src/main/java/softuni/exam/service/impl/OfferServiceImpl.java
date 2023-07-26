e9aea1124259c73754a6fb7a2d6716b852686c52
danovd <danovski_dimitar@abv.bg>
2023-07-26 11:33:11 +0300

    read offers.xml functionality done

--- EXAMS/DB_EXAM_29Mar2020_RealDeal/real_deal/src/main/java/softuni/exam/service/impl/OfferServiceImpl.java
+++ EXAMS/DB_EXAM_29Mar2020_RealDeal/real_deal/src/main/java/softuni/exam/service/impl/OfferServiceImpl.java
@@ -1,20 +1,40 @@
 package softuni.exam.service.impl;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
+import softuni.exam.repository.OfferRepository;
 import softuni.exam.service.OfferService;
+import softuni.exam.util.ValidationUtil;
+import softuni.exam.util.XmlParser;
 
 import javax.xml.bind.JAXBException;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 @Service
 public class OfferServiceImpl implements OfferService {
+    private static final String OFFERS_FILE_PATH = "src/main/resources/files/xml/offers.xml";
+    private final OfferRepository offerRepository;
+    private final ModelMapper modelMapper;
+    private final XmlParser xmlParser;
+    private final ValidationUtil validator;
+
+    public OfferServiceImpl(OfferRepository offerRepository, ModelMapper modelMapper, XmlParser xmlParser, ValidationUtil validator) {
+        this.offerRepository = offerRepository;
+        this.modelMapper = modelMapper;
+        this.xmlParser = xmlParser;
+        this.validator = validator;
+    }
+
     @Override
     public boolean areImported() {
-        return false;
+        return offerRepository.count() > 0;
     }
 
     @Override
     public String readOffersFileContent() throws IOException {
-        return null;
+        return Files.readString(Path.of(OFFERS_FILE_PATH));
     }
 
     @Override
