c429c12e7071e4653ebe961a32d06fb7e51781e6
danovd <danovski_dimitar@abv.bg>
2023-07-27 11:18:59 +0300

    players fields for autowire added

--- EXAMS/DB_EXAM_28Jul2019_Football/football/src/main/java/softuni/exam/service/PlayerServiceImpl.java
+++ EXAMS/DB_EXAM_28Jul2019_Football/football/src/main/java/softuni/exam/service/PlayerServiceImpl.java
@@ -1,9 +1,32 @@
 package softuni.exam.service;
 
+import com.google.gson.Gson;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
+import softuni.exam.repository.PlayerRepository;
+import softuni.exam.util.FileUtil;
+
+import javax.validation.Validator;
 
 @Service
 public class PlayerServiceImpl implements PlayerService {
+
+
+    private static final String PLAYERS_FILE_PATH = "src/main/resources/files/json/players.json";
+
+    private final PlayerRepository playerRepository;
+    private final ModelMapper modelMapper;
+    private final Gson gson;
+    private final Validator validator;
+    private final FileUtil fileUtil;
+
+    public PlayerServiceImpl(PlayerRepository playerRepository, ModelMapper modelMapper, Gson gson, Validator validator, FileUtil fileUtil) {
+        this.playerRepository = playerRepository;
+        this.modelMapper = modelMapper;
+        this.gson = gson;
+        this.validator = validator;
+        this.fileUtil = fileUtil;
+    }
 
     @Override
     public String importPlayers() {
