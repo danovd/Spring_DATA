d710e242841a69945f6519f24e29b900c2f01880
danovd <danovski_dimitar@abv.bg>
2023-07-27 11:11:29 +0300

    pictures and teams fields for autowire added

--- EXAMS/DB_EXAM_28Jul2019_Football/football/src/main/java/softuni/exam/service/PictureServiceImpl.java
+++ EXAMS/DB_EXAM_28Jul2019_Football/football/src/main/java/softuni/exam/service/PictureServiceImpl.java
@@ -1,9 +1,33 @@
 package softuni.exam.service;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
+import softuni.exam.repository.PictureRepository;
+import softuni.exam.util.FileUtil;
+import softuni.exam.util.XmlParser;
+
+import javax.validation.Validator;
 
 @Service
 public class PictureServiceImpl implements PictureService {
+
+    private static final String PICTURE_FILE_PATH = "src/main/resources/files/xml/pictures.xml";
+
+    private final PictureRepository pictureRepository;
+    private final ModelMapper modelMapper;
+    private final Validator validator;
+    private final XmlParser xmlParser;
+    private final FileUtil fileUtil;
+
+    public PictureServiceImpl(PictureRepository pictureRepository, ModelMapper modelMapper, Validator validator, XmlParser xmlParser, FileUtil fileUtil) {
+        this.pictureRepository = pictureRepository;
+        this.modelMapper = modelMapper;
+        this.validator = validator;
+        this.xmlParser = xmlParser;
+        this.fileUtil = fileUtil;
+    }
+
+
     @Override
     public String importPictures() {
         //TODO Implement me
@@ -12,8 +36,7 @@ public class PictureServiceImpl implements PictureService {
 
     @Override
     public boolean areImported() {
-        //TODO Implement me
-        return false;
+        return pictureRepository.count() > 0;
     }
 
     @Override
--- EXAMS/DB_EXAM_28Jul2019_Football/football/src/main/java/softuni/exam/service/TeamServiceImpl.java
+++ EXAMS/DB_EXAM_28Jul2019_Football/football/src/main/java/softuni/exam/service/TeamServiceImpl.java
@@ -1,9 +1,31 @@
 package softuni.exam.service;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
+import softuni.exam.repository.TeamRepository;
+import softuni.exam.util.FileUtil;
+import softuni.exam.util.XmlParser;
+
+import javax.validation.Validator;
 
 @Service
 public class TeamServiceImpl implements TeamService {
+
+
+    private final TeamRepository teamRepository;
+    private final ModelMapper modelMapper;
+    private final Validator validator;
+    private final XmlParser xmlParser;
+    private final FileUtil fileUtil;
+
+    public TeamServiceImpl(TeamRepository teamRepository, ModelMapper modelMapper, Validator validator, XmlParser xmlParser, FileUtil fileUtil) {
+        this.teamRepository = teamRepository;
+        this.modelMapper = modelMapper;
+        this.validator = validator;
+        this.xmlParser = xmlParser;
+        this.fileUtil = fileUtil;
+    }
+
 
     @Override
     public String importTeams() {
@@ -13,8 +35,7 @@ public class TeamServiceImpl implements TeamService {
 
     @Override
     public boolean areImported() {
-        //TODO Implement me
-        return false;
+        return teamRepository.count() > 0;
     }
 
     @Override
