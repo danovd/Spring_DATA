7c2da0632f446d0be66eb9d59d4e1c0f05084d92
danovd <danovski_dimitar@abv.bg>
2023-07-28 11:25:03 +0300

    EmployeeCard entity released

--- EXAMS/DB_EXAM_20Dec2018_Hiberspring/hiberspring/src/main/java/hiberspring/domain/entities/Employee.java
+++ EXAMS/DB_EXAM_20Dec2018_Hiberspring/hiberspring/src/main/java/hiberspring/domain/entities/Employee.java
@@ -1,4 +1,75 @@
 package hiberspring.domain.entities;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "employees")
 public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(nullable = false)
+    private String firstName;
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false)
+    private String position;
+    @OneToOne
+    @Column(nullable = false, unique = true)
+    private EmployeeCard employeeCard;
+    @ManyToOne
+    @Column(nullable = false)
+    private Branch branch;
+
+    public Employee() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public EmployeeCard getEmployeeCard() {
+        return employeeCard;
+    }
+
+    public void setEmployeeCard(EmployeeCard employeeCard) {
+        this.employeeCard = employeeCard;
+    }
+
+    public Branch getBranch() {
+        return branch;
+    }
+
+    public void setBranch(Branch branch) {
+        this.branch = branch;
+    }
 }
--- EXAMS/DB_EXAM_20Dec2018_Hiberspring/hiberspring/src/main/java/hiberspring/domain/entities/EmployeeCard.java
+++ EXAMS/DB_EXAM_20Dec2018_Hiberspring/hiberspring/src/main/java/hiberspring/domain/entities/EmployeeCard.java
@@ -1,4 +1,9 @@
 package hiberspring.domain.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "employee_cards")
 public class EmployeeCard {
 }
