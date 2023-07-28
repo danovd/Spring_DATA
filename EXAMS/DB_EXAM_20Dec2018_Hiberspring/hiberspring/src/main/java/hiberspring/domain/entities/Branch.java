32d94c2202218ea7e7df43ad60500b91b7617c1a
danovd <danovski_dimitar@abv.bg>
2023-07-28 11:17:22 +0300

    Branch entity made

--- EXAMS/DB_EXAM_20Dec2018_Hiberspring/hiberspring/src/main/java/hiberspring/domain/entities/Branch.java
+++ EXAMS/DB_EXAM_20Dec2018_Hiberspring/hiberspring/src/main/java/hiberspring/domain/entities/Branch.java
@@ -1,4 +1,44 @@
 package hiberspring.domain.entities;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "branches")
 public class Branch {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false)
+    private String name;
+    @ManyToOne(optional = false)
+    private Town town;
+
+
+    public Branch() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Town getTown() {
+        return town;
+    }
+
+    public void setTown(Town town) {
+        this.town = town;
+    }
 }
--- EXAMS/DB_EXAM_20Dec2018_Hiberspring/hiberspring/src/main/java/hiberspring/domain/entities/Town.java
+++ EXAMS/DB_EXAM_20Dec2018_Hiberspring/hiberspring/src/main/java/hiberspring/domain/entities/Town.java
@@ -1,4 +1,12 @@
 package hiberspring.domain.entities;
 
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+
+@Entity
+@Table(name = "towns")
 public class Town {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
 }
