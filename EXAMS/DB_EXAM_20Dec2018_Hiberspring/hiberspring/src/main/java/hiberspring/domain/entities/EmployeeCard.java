313c4bd958fc71a5e1cd2949c5db652bcd58d02c
danovd <danovski_dimitar@abv.bg>
2023-07-28 11:26:56 +0300

    Employee and EmployeeCard entities finished

--- EXAMS/DB_EXAM_20Dec2018_Hiberspring/hiberspring/src/main/java/hiberspring/domain/entities/EmployeeCard.java
+++ EXAMS/DB_EXAM_20Dec2018_Hiberspring/hiberspring/src/main/java/hiberspring/domain/entities/EmployeeCard.java
@@ -1,9 +1,33 @@
 package hiberspring.domain.entities;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "employee_cards")
 public class EmployeeCard {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(unique = true)
+    private String number;
+
+    public EmployeeCard() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
 }
