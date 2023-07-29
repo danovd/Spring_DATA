af28951a23f7664389e9e8ce6d90290155e551be
danovd <danovski_dimitar@abv.bg>
2023-07-29 07:54:43 +0300

    read teams functionality added

--- EXAMS/DB_EXAM_01Aug2021_PlayerFinder/player_finder/src/main/java/com/example/football/service/TeamService.java
+++ EXAMS/DB_EXAM_01Aug2021_PlayerFinder/player_finder/src/main/java/com/example/football/service/TeamService.java
@@ -1,10 +1,12 @@
 package com.example.football.service;
 
+import java.io.IOException;
+
 //ToDo - Implement all methods
 public interface TeamService {
     boolean areImported();
 
-    String readTeamsFileContent() ;
+    String readTeamsFileContent() throws IOException;
 
     String importTeams() ;
 
--- EXAMS/DB_EXAM_01Aug2021_PlayerFinder/player_finder/src/main/java/com/example/football/service/impl/TeamServiceImpl.java
+++ EXAMS/DB_EXAM_01Aug2021_PlayerFinder/player_finder/src/main/java/com/example/football/service/impl/TeamServiceImpl.java
@@ -1,20 +1,41 @@
 package com.example.football.service.impl;
 
+import com.example.football.repository.TeamRepository;
 import com.example.football.service.TeamService;
+import com.example.football.util.FileUtil;
+import com.example.football.util.ValidationUtil;
+import com.google.gson.Gson;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
+
+import java.io.IOException;
 
 @Service
 public class TeamServiceImpl implements TeamService {
 
+    private static final String TEAMS_FILE_PATH = "src/main/resources/files/json/teams.json";
+    private final TeamRepository teamRepository;
+    private final ModelMapper modelMapper;
+    private final Gson gson;
+    private final ValidationUtil validator;
+    private final FileUtil fileUtil;
 
-    @Override
-    public boolean areImported() {
-        return false;
+    public TeamServiceImpl(TeamRepository teamRepository, ModelMapper modelMapper, Gson gson, ValidationUtil validator, FileUtil fileUtil) {
+        this.teamRepository = teamRepository;
+        this.modelMapper = modelMapper;
+        this.gson = gson;
+        this.validator = validator;
+        this.fileUtil = fileUtil;
     }
 
     @Override
-    public String readTeamsFileContent() {
-        return null;
+    public boolean areImported() {
+        return this.teamRepository.count() > 0;
+    }
+
+    @Override
+    public String readTeamsFileContent() throws IOException {
+        return fileUtil.readFile(TEAMS_FILE_PATH);
     }
 
     @Override
