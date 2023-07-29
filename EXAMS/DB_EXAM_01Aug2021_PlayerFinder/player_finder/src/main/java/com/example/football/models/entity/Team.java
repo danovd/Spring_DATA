2fa8982e3543d87bf8b3cb27ff2fccd24f20ff82
danovd <danovski_dimitar@abv.bg>
2023-07-29 07:51:10 +0300

    teams entity added

--- EXAMS/DB_EXAM_01Aug2021_PlayerFinder/player_finder/src/main/java/com/example/football/models/entity/Team.java
+++ EXAMS/DB_EXAM_01Aug2021_PlayerFinder/player_finder/src/main/java/com/example/football/models/entity/Team.java
@@ -2,10 +2,48 @@ package com.example.football.models.entity;
 
 import javax.persistence.*;
 
+
 @Entity
 @Table(name = "teams")
 public class Team {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
+    @Column(nullable = false)
+    private int fanBase;
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String history;
+    @Column(nullable = false, unique = true)
+    private String name;
+    @Column(nullable = false)
+    private String stadiumName;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
