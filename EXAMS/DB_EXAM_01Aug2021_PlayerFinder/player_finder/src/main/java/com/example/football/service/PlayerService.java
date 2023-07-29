f9b911f57121da3fc9a301ceef83697d165a8eb3
danovd <danovski_dimitar@abv.bg>
2023-07-29 07:55:41 +0300

    read players functionality added

--- EXAMS/DB_EXAM_01Aug2021_PlayerFinder/player_finder/src/main/java/com/example/football/service/PlayerService.java
+++ EXAMS/DB_EXAM_01Aug2021_PlayerFinder/player_finder/src/main/java/com/example/football/service/PlayerService.java
@@ -1,10 +1,12 @@
 package com.example.football.service;
 
+import java.io.IOException;
+
 //ToDo - Implement all methods
 public interface PlayerService {
     boolean areImported();
 
-    String readPlayersFileContent() ;
+    String readPlayersFileContent() throws IOException;
 
     String importPlayers() ;
 
--- EXAMS/DB_EXAM_01Aug2021_PlayerFinder/player_finder/src/main/java/com/example/football/service/impl/PlayerServiceImpl.java
+++ EXAMS/DB_EXAM_01Aug2021_PlayerFinder/player_finder/src/main/java/com/example/football/service/impl/PlayerServiceImpl.java
@@ -1,20 +1,41 @@
 package com.example.football.service.impl;
 
+import com.example.football.repository.PlayerRepository;
 import com.example.football.service.PlayerService;
+import com.example.football.util.FileUtil;
+import com.example.football.util.ValidationUtil;
+import com.example.football.util.XmlParser;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
+
+import java.io.IOException;
 
 @Service
 public class PlayerServiceImpl implements PlayerService {
 
+    private static final String PLAYERS_FILE_PATH = "src/main/resources/files/xml/players.xml";
+    private final PlayerRepository playerRepository;
+    private final ModelMapper modelMapper;
+    private final XmlParser xmlParser;
+    private final ValidationUtil validator;
+    private final FileUtil fileUtil;
 
-    @Override
-    public boolean areImported() {
-        return false;
+    public PlayerServiceImpl(PlayerRepository playerRepository, ModelMapper modelMapper, XmlParser xmlParser, ValidationUtil validator, FileUtil fileUtil) {
+        this.playerRepository = playerRepository;
+        this.modelMapper = modelMapper;
+        this.xmlParser = xmlParser;
+        this.validator = validator;
+        this.fileUtil = fileUtil;
     }
 
     @Override
-    public String readPlayersFileContent()  {
-        return null;
+    public boolean areImported() {
+        return this.playerRepository.count() > 0;
+    }
+
+    @Override
+    public String readPlayersFileContent() throws IOException {
+        return fileUtil.readFile(PLAYERS_FILE_PATH);
     }
 
     @Override
