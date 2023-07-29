51d1f1d5a717d79e5608dd84d61cc170efad79f8
danovd <danovski_dimitar@abv.bg>
2023-07-29 07:52:57 +0300

    player entity added

--- EXAMS/DB_EXAM_01Aug2021_PlayerFinder/player_finder/src/main/java/com/example/football/models/entity/Player.java
+++ EXAMS/DB_EXAM_01Aug2021_PlayerFinder/player_finder/src/main/java/com/example/football/models/entity/Player.java
@@ -1,6 +1,8 @@
 package com.example.football.models.entity;
 
 import javax.persistence.*;
+import javax.validation.constraints.Email;
+import java.time.LocalDate;
 
 @Entity
 @Table(name = "players")
@@ -8,4 +10,54 @@ public class Player {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+    @Column(nullable = false)
+    private String firstName;
+    @Column(nullable = false)
+    private String lastName;
+    @Column(nullable = false, unique = true)
+    private String email;
+    @Column(nullable = false)
+    private LocalDate birthDate;
+    @Column(nullable = false)
+    private PlayerPosition position;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
---
+++ EXAMS/DB_EXAM_01Aug2021_PlayerFinder/player_finder/src/main/java/com/example/football/models/entity/PlayerPosition.java
@@ -0,0 +1,5 @@
+package com.example.football.models.entity;
+
+public enum PlayerPosition {
+    ATT, MID, DEF
+}
