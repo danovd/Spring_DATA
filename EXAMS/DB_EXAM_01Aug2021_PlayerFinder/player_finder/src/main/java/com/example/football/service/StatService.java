3a338356f7d3e1dc382938082c9988e7ca9bb9f5
danovd <danovski_dimitar@abv.bg>
2023-07-29 07:56:08 +0300

    read stats functionality added

--- EXAMS/DB_EXAM_01Aug2021_PlayerFinder/player_finder/src/main/java/com/example/football/service/StatService.java
+++ EXAMS/DB_EXAM_01Aug2021_PlayerFinder/player_finder/src/main/java/com/example/football/service/StatService.java
@@ -1,10 +1,12 @@
 package com.example.football.service;
 
+import java.io.IOException;
+
 //ToDo - Implement all methods
 public interface StatService {
     boolean areImported();
 
-    String readStatsFileContent() ;
+    String readStatsFileContent() throws IOException;
 
     String importStats() ;
 
--- EXAMS/DB_EXAM_01Aug2021_PlayerFinder/player_finder/src/main/java/com/example/football/service/impl/StatServiceImpl.java
+++ EXAMS/DB_EXAM_01Aug2021_PlayerFinder/player_finder/src/main/java/com/example/football/service/impl/StatServiceImpl.java
@@ -1,20 +1,41 @@
 package com.example.football.service.impl;
 
+import com.example.football.repository.StatRepository;
 import com.example.football.service.StatService;
+import com.example.football.util.FileUtil;
+import com.example.football.util.ValidationUtil;
+import com.example.football.util.XmlParser;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
+
+import java.io.IOException;
 
 @Service
 public class StatServiceImpl implements StatService {
 
+    private static final String STATS_FILE_PATH = "src/main/resources/files/xml/stats.xml";
+    private final StatRepository statRepository;
+    private final ModelMapper modelMapper;
+    private final XmlParser xmlParser;
+    private final ValidationUtil validator;
+    private final FileUtil fileUtil;
 
-    @Override
-    public boolean areImported() {
-        return false;
+    public StatServiceImpl(StatRepository statRepository, ModelMapper modelMapper, XmlParser xmlParser, ValidationUtil validator, FileUtil fileUtil) {
+        this.statRepository = statRepository;
+        this.modelMapper = modelMapper;
+        this.xmlParser = xmlParser;
+        this.validator = validator;
+        this.fileUtil = fileUtil;
     }
 
     @Override
-    public String readStatsFileContent()  {
-        return null;
+    public boolean areImported() {
+        return this.statRepository.count() > 0;
+    }
+
+    @Override
+    public String readStatsFileContent() throws IOException {
+        return fileUtil.readFile(STATS_FILE_PATH);
     }
 
     @Override
