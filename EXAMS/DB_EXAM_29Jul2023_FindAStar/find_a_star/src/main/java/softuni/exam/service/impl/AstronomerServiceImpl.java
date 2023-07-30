422a8b1516428639db90d538fb64be0d0b1d85c4
danovd <danovski_dimitar@abv.bg>
2023-07-30 10:22:07 +0300

    astronomer service completed

--- EXAMS/DB_EXAM_29Jul2023_FindAStar/find_a_star/src/main/java/softuni/exam/service/impl/AstronomerServiceImpl.java
+++ EXAMS/DB_EXAM_29Jul2023_FindAStar/find_a_star/src/main/java/softuni/exam/service/impl/AstronomerServiceImpl.java
@@ -2,6 +2,11 @@ package softuni.exam.service.impl;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import softuni.exam.models.dto.ImportAstronomerDto;
+import softuni.exam.models.dto.ImportAstronomerRootDTO;
+import softuni.exam.models.entity.Astronomer;
+import softuni.exam.models.entity.Star;
 import softuni.exam.repository.AstronomerRepository;
 import softuni.exam.repository.StarRepository;
 import softuni.exam.service.AstronomerService;
@@ -11,8 +16,12 @@ import softuni.exam.util.XmlParser;
 
 import javax.xml.bind.JAXBException;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
-
+@Service
 public class AstronomerServiceImpl implements AstronomerService {
 
 
@@ -24,7 +33,7 @@ public class AstronomerServiceImpl implements AstronomerService {
     private final FileUtil fileUtil;
     private final StarRepository starRepository;
 
-
+    @Autowired
     public AstronomerServiceImpl(AstronomerRepository astronomerRepository, ModelMapper modelMapper, XmlParser xmlParser, ValidationUtil validator, FileUtil fileUtil, StarRepository starRepository) {
         this.astronomerRepository = astronomerRepository;
         this.modelMapper = modelMapper;
@@ -33,18 +42,57 @@ public class AstronomerServiceImpl implements AstronomerService {
         this.fileUtil = fileUtil;
         this.starRepository = starRepository;
     }
+
     @Override
     public boolean areImported() {
-        return false;
+
+        return this.astronomerRepository.count() > 0;
     }
 
     @Override
     public String readAstronomersFromFile() throws IOException {
-        return null;
+
+        return Files.readString(Path.of(ASTRONOMERS_FILE_PATH));
+        //  return fileUtil.readFile(ASTRONOMERS_FILE_PATH);
     }
 
     @Override
     public String importAstronomers() throws IOException, JAXBException {
-        return null;
+        ImportAstronomerRootDTO statRootDTOs = this.xmlParser.parseXml(ASTRONOMERS_FILE_PATH, ImportAstronomerRootDTO.class);
+        return statRootDTOs.getAstronomers().stream().map(this::importDTO).collect(Collectors.joining("\n"));
+    }
+
+    private String importDTO(ImportAstronomerDto dto) {
+        boolean isValid = this.validator.isValid(dto);
+
+        if (!isValid) {
+            return "Invalid astronomer";
+        }
+
+        Optional<Astronomer> optAstronomer = this.astronomerRepository.findByFirstNameAndLastName(dto.getFirstName(), dto.getLastName());
+
+
+        if (optAstronomer.isPresent()) {
+            return "Invalid astronomer";
+        }
+
+
+        Astronomer astronomer = this.modelMapper.map(dto, Astronomer.class);
+
+        // Find and set a star
+        Optional<Star> star = this.starRepository.findById(dto.getObservingStar());
+
+        if(star.isEmpty()){
+            return "Invalid astronomer";
+        }
+        astronomer.setObservingStar(star.get());
+        ///
+
+
+        this.astronomerRepository.save(astronomer);
+
+        return String.format("Successfully imported astronomer %s %s - %.2f",
+                dto.getFirstName(), dto.getLastName(), dto.getAverageObservationHours());
     }
 }
+
