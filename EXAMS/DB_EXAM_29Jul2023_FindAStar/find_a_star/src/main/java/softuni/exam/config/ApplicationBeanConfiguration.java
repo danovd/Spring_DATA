fa2b9539c2abc909f115a1ca4ea87b3f6e5f57d0
danovd <danovski_dimitar@abv.bg>
2023-07-30 10:11:04 +0300

    configuration finalized

--- EXAMS/DB_EXAM_29Jul2023_FindAStar/find_a_star/src/main/java/softuni/exam/config/ApplicationBeanConfiguration.java
+++ EXAMS/DB_EXAM_29Jul2023_FindAStar/find_a_star/src/main/java/softuni/exam/config/ApplicationBeanConfiguration.java
@@ -1,6 +1,56 @@
 package softuni.exam.config;
 
-// TODO:
-public class ApplicationBeanConfiguration {
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import org.modelmapper.Converter;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.spi.MappingContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import softuni.exam.util.*;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+@Configuration
+public class ApplicationBeanConfiguration {
+    @Bean
+    public FileUtil fileUtil() {
+        return new FileUtilImpl();
+    }
+
+    @Bean
+    public Gson gson() {
+        return new GsonBuilder()
+                //.excludeFieldsWithoutExposeAnnotation()
+                .setPrettyPrinting()
+                .create();
+    }
+
+    @Bean
+    public XmlParser xmlParser(){
+        return new XmlParserImpl();
+    }
+
+    @Bean
+    public ValidationUtil validationUtil() {
+        return new ValidationUtilImpl();
+    }
+
+    @Bean
+    public ModelMapper modelMapper() {
+
+        ModelMapper modelMapper = new ModelMapper();
+
+        modelMapper.addConverter(new Converter<String, LocalDate>() {
+            @Override
+            public LocalDate convert(MappingContext<String, LocalDate> mappingContext) {
+                return LocalDate.parse(mappingContext.getSource(), DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+
+            }
+        });
+
+
+        return modelMapper;
+    }
 }
