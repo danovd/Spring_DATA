96f1e4ff335e34c292cffc78f39b319eab44d0d3
danovd <danovski_dimitar@abv.bg>
2023-07-30 10:09:27 +0300

    constellation service initiate

--- EXAMS/DB_EXAM_29Jul2023_FindAStar/find_a_star/src/main/java/softuni/exam/service/impl/ConstellationServiceImpl.java
+++ EXAMS/DB_EXAM_29Jul2023_FindAStar/find_a_star/src/main/java/softuni/exam/service/impl/ConstellationServiceImpl.java
@@ -1,6 +1,50 @@
 package softuni.exam.service.impl;
 
-// TODO: Implement all methods
-public class ConstellationServiceImpl {
+import com.google.gson.Gson;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import softuni.exam.repository.ConstellationRepository;
+import softuni.exam.service.ConstellationService;
+
+
+import java.io.IOException;
+
+
+@Service
+public class ConstellationServiceImpl implements ConstellationService {
+
+    private static final String CONSTELLATIONS_FILE_PATH = "src/main/resources/files/json/constellations.json";
+    private final ConstellationRepository constellationRepository;
+    private final ModelMapper modelMapper;
+    private final Gson gson;
+  // private final ValidationUtil validator;
+  //  private final FileUtil fileUtil;
+
+    @Autowired
+    public ConstellationServiceImpl(ConstellationRepository constellationRepository, ModelMapper modelMapper, Gson gson) {
+        this.constellationRepository = constellationRepository;
+        this.modelMapper = modelMapper;
+        this.gson = gson;
+
+    }
+
+
+    @Override
+    public boolean areImported() {
+        return false;
+    }
+
+    @Override
+    public String readConstellationsFromFile() throws IOException {
+        return null;
+    }
+
+    @Override
+    public String importConstellations() throws IOException {
+      return null;
+    }
+
 
 }
