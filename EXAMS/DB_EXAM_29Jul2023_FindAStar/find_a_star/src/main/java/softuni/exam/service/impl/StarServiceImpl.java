cb1c4088ffdb1060ce020a5f1f05e7cf37f30ca7
danovd <danovski_dimitar@abv.bg>
2023-07-30 10:16:22 +0300

    star service all fields added

--- EXAMS/DB_EXAM_29Jul2023_FindAStar/find_a_star/src/main/java/softuni/exam/service/impl/StarServiceImpl.java
+++ EXAMS/DB_EXAM_29Jul2023_FindAStar/find_a_star/src/main/java/softuni/exam/service/impl/StarServiceImpl.java
@@ -1,6 +1,58 @@
 package softuni.exam.service.impl;
 
-// TODO: Implement all methods
-public class StarServiceImpl {
+import com.google.gson.Gson;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import softuni.exam.repository.AstronomerRepository;
+import softuni.exam.repository.ConstellationRepository;
+import softuni.exam.repository.StarRepository;
+import softuni.exam.service.StarService;
+import softuni.exam.util.FileUtil;
+import softuni.exam.util.ValidationUtil;
 
+import java.io.IOException;
+
+public class StarServiceImpl implements StarService {
+    private static final String STARS_FILE_PATH = "src/main/resources/files/json/stars.json";
+    private final StarRepository starRepository;
+    private final ModelMapper modelMapper;
+    private final Gson gson;
+    private final ValidationUtil validator;
+    private final FileUtil fileUtil;
+
+    private final AstronomerRepository astronomerRepository;
+
+    private final ConstellationRepository constellationRepository;
+    
+    public StarServiceImpl(StarRepository starRepository, ModelMapper modelMapper, Gson gson, ValidationUtil validator, FileUtil fileUtil, AstronomerRepository astronomerRepository, ConstellationRepository constellationRepository) {
+        this.starRepository = starRepository;
+        this.modelMapper = modelMapper;
+        this.gson = gson;
+        this.validator = validator;
+        this.fileUtil = fileUtil;
+        this.astronomerRepository = astronomerRepository;
+        this.constellationRepository = constellationRepository;
+
+
+
+    }
+    @Override
+    public boolean areImported() {
+        return false;
+    }
+
+    @Override
+    public String readStarsFileContent() throws IOException {
+        return null;
+    }
+
+    @Override
+    public String importStars() throws IOException {
+        return null;
+    }
+
+    @Override
+    public String exportStars() {
+        return null;
+    }
 }
